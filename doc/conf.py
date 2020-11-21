9d7387d7e3cbc79e8c3665281b5a277b2c277c55
Thomas Wucher <mail@thomaswucher.de>
2020-11-21 11:52:00 +0100

    Add custom CSS for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -63,3 +63,5 @@ html_theme = "sphinx_rtd_theme"
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
 html_static_path = ["_static"]
+
+html_css_files = ["css/custom.css"]
