7368207139ac46336f055121516f07d6c50d69af
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-04 11:56:15 +0100

    Merge/Split all error and gamma functions
    Includes:
    erfd.c, erfcd.c, lgammad.c, tgammad.c
    erff.c, erfcf.c, lgammaf.c, tgammaf.c

--- libm/mathd/lgammad.c
+++ libm/mathd/lgammad.c
@@ -1,4 +1,3 @@
-
 /* @(#)er_lgamma.c 5.1 93/09/24 */
 /*
  * ====================================================
@@ -307,3 +306,58 @@ static double zero=  0.00000000000000000000e+00;
 	if(hx<0) r = nadj - r;
 	return r;
 }
+
+/* @(#)w_lgamma.c 5.1 93/09/24 */
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice 
+ * is preserved.
+ * ====================================================
+ *
+ */
+
+/* double lgamma(double x)
+ * Return the logarithm of the Gamma function of x.
+ *
+ * Method: call __ieee754_lgamma_r
+ */
+
+#include "fdlibm.h"
+#include <reent.h>
+#include <errno.h>
+
+#ifndef _DOUBLE_IS_32BITS
+
+#ifdef __STDC__
+	double lgamma(double x)
+#else
+	double lgamma(x)
+	double x;
+#endif
+{
+#ifdef _IEEE_LIBM
+	return __ieee754_lgamma_r(x,&(_REENT_SIGNGAM(_REENT)));
+#else
+        double y;
+        y = __ieee754_lgamma_r(x,&(_REENT_SIGNGAM(_REENT)));
+        if(_LIB_VERSION == _IEEE_) return y;
+        if(!finite(y)&&finite(x)) {
+	    /* lgamma(finite) overflow */
+	    errno = ERANGE;
+	}
+	return y;
+#endif
+}             
+
+#endif /* defined(_DOUBLE_IS_32BITS) */
+
+
+
+
+
+
+
