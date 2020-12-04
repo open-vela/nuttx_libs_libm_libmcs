531c0b3da727cb6637d46b74dd179ee200d1e339
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-04 16:52:02 +0100

    Add SPDX to nearest integer functions

--- libm/mathd/rintd.c
+++ libm/mathd/rintd.c
@@ -1,15 +1,6 @@
+// SPDX-License-Identifier: SunMicrosystems
+// Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
 
-/* @(#)s_rint.c 5.1 93/09/24 */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
- * is preserved.
- * ====================================================
- */
 /*
 FUNCTION
 <<rint>>, <<rintf>>---round to integer
