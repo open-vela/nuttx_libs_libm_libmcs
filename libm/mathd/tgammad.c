c1dae6479101d2d1ffbec37636ee4e7bc3913cf8
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-04 16:43:09 +0100

    Add SPDX to error and gamma functions

--- libm/mathd/tgammad.c
+++ libm/mathd/tgammad.c
@@ -1,15 +1,5 @@
-/* @(#)e_tgamma.c 5.1 93/09/24 */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
- * is preserved.
- * ====================================================
- *
- */
+// SPDX-License-Identifier: SunMicrosystems
+// Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
 
 /* __ieee754_tgamma(x)
  * Gamma function. Returns gamma(x)
@@ -32,18 +22,6 @@
 		y = -y;
 	return y;
 }
-
-/* @(#)w_gamma.c 5.1 93/09/24 */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
 
 /* double gamma(double x)
  * Return  the logarithm of the Gamma function of x or the Gamma function of x,
