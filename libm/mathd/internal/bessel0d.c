16023e40600f67e3fc4e9fa485f7484586c964a8
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-04 18:06:12 +0100

    Add SPDX to bessel functions

--- libm/mathd/internal/bessel0d.c
+++ libm/mathd/internal/bessel0d.c
@@ -1,15 +1,5 @@
-
-/* @(#)e_j0.c 5.1 93/09/24 */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
- * is preserved.
- * ====================================================
- */
+// SPDX-License-Identifier: SunMicrosystems
+// Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
 
 /* __ieee754_j0(x), __ieee754_y0(x)
  * Bessel function of the first and second kinds of order zero.
