f50956956d7a870be2c2a0418580debeb7f9b9ed
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-04 16:43:09 +0100

    Add SPDX to error and gamma functions

--- libm/mathf/tgammaf.c
+++ libm/mathf/tgammaf.c
@@ -1,18 +1,6 @@
-/* ef_tgamma.c -- float version of e_tgamma.c.
- * Conversion to float by Ian Lance Taylor, Cygnus Support, ian@cygnus.com.
- */
-
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
- * is preserved.
- * ====================================================
- *
- */
+// SPDX-License-Identifier: SunMicrosystems
+// Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+// Conversion to float by Ian Lance Taylor, Cygnus Support, ian@cygnus.com.
 
 /* __ieee754_tgammaf(x)
  * Float version the Gamma function. Returns gamma(x)
@@ -34,21 +22,6 @@
 		y = -y;
 	return y;
 }
-
-/* w_gammaf.c -- float version of w_gamma.c.
- * Conversion to float by Ian Lance Taylor, Cygnus Support, ian@cygnus.com.
- */
-
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
 
 #include "math.h"
 #include "fdlibm.h"
