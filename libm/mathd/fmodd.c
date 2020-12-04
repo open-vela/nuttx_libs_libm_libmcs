4ab0538ebe42607df9b488c5858416e3da23ced8
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-04 17:48:31 +0100

    Add SPDX to remainder functions

--- libm/mathd/fmodd.c
+++ libm/mathd/fmodd.c
@@ -1,14 +1,5 @@
-/* @(#)e_fmod.c 5.1 93/09/24 */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
- * is preserved.
- * ====================================================
- */
+// SPDX-License-Identifier: SunMicrosystems
+// Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
 
 /* 
  * __ieee754_fmod(x,y)
@@ -137,18 +128,6 @@ static double one = 1.0, Zero[] = {0.0, -0.0,};
 }
 
 #endif /* defined(_DOUBLE_IS_32BITS) */
-
-/* @(#)w_fmod.c 5.1 93/09/24 */
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice 
- * is preserved.
- * ====================================================
- */
 
 /* 
 FUNCTION 
