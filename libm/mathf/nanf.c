a2dac40b4b9794a2b907a347704a9450bb585604
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-08 15:09:44 +0100

    Add content to new implementation files

--- libm/mathf/nanf.c
+++ libm/mathf/nanf.c
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: GTDGmbH
 // Copyright 2020-2021 by GTD GmbH.
+
+float nanf (const char * payload)
+{
+    return 0.0f/0.0f;
+}
+
+#ifdef _DOUBLE_IS_32BITS
+
+double nan (const char * payload)
+{
+	return (double) nanf(payload);
+}
+
+#endif /* defined(_DOUBLE_IS_32BITS) */
