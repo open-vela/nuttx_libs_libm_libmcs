289c0ed208eb0da342cdc33154cdbc69dadb6af9
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-10 14:10:45 +0100

    Format files in include

--- libm/include/fenv.h
+++ libm/include/fenv.h
@@ -6,20 +6,20 @@
 #error TBD add error message that fenv.h/fenv.c shall not be used as is.
 
 // Floating-point Exceptions
-extern int feclearexcept(int excepts);
-extern int feraiseexcept(int excepts);
-extern int fegetexceptflag(fexcept_t* flagp, int excepts);
-extern int fesetexceptflag(const fexcept_t* flagp, int excepts);
+extern int feclearexcept(int);
+extern int feraiseexcept(int);
+extern int fegetexceptflag(fexcept_t *, int);
+extern int fesetexceptflag(const fexcept_t *, int);
 
 // Rounding Direction
 extern int fegetround();
-extern int fesetround(int rdir);
+extern int fesetround(int);
 
 // Entire Environment
-extern int fegetenv(fenv_t* envp);
-extern int fesetenv(const fenv_t* envp);
-extern int feholdexcept(fenv_t* envp);
-extern int feupdateenv(const fenv_t* envp);
+extern int fegetenv(fenv_t *);
+extern int fesetenv(const fenv_t *);
+extern int feholdexcept(fenv_t *);
+extern int feupdateenv(const fenv_t *);
 
 // Other
-extern int fetestexcept(int excepts);
+extern int fetestexcept(int);
