de4f480f97c95e4b21b8b4e0bfcd938a336215cb
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2020-12-10 12:24:24 +0100

    Format files in common

--- libm/common/tools.h
+++ libm/common/tools.h
@@ -11,125 +11,120 @@
 
 #ifdef __IEEE_BIG_ENDIAN
 
-typedef union 
-{
-  double value;
-  struct 
-  {
-    __uint32_t msw;
-    __uint32_t lsw;
-  } parts;
+typedef union {
+    double value;
+    struct {
+        __uint32_t msw;
+        __uint32_t lsw;
+    } parts;
 } ieee_double_shape_type;
 
 #endif
 
 #ifdef __IEEE_LITTLE_ENDIAN
 
-typedef union 
-{
-  double value;
-  struct 
-  {
-    __uint32_t lsw;
-    __uint32_t msw;
-  } parts;
+typedef union {
+    double value;
+    struct {
+        __uint32_t lsw;
+        __uint32_t msw;
+    } parts;
 } ieee_double_shape_type;
 
 #endif
 
 /* Get two 32 bit ints from a double.  */
 
-#define EXTRACT_WORDS(ix0,ix1,d)                \
-do {                                \
-  ieee_double_shape_type ew_u;                    \
-  ew_u.value = (d);                        \
-  (ix0) = ew_u.parts.msw;                    \
-  (ix1) = ew_u.parts.lsw;                    \
-} while (0)
+#define EXTRACT_WORDS(ix0,ix1,d)                        \
+    do {                                                \
+        ieee_double_shape_type ew_u;                    \
+        ew_u.value = (d);                               \
+        (ix0) = ew_u.parts.msw;                         \
+        (ix1) = ew_u.parts.lsw;                         \
+    } while (0)
 
 /* Get the more significant 32 bit int from a double.  */
 
-#define GET_HIGH_WORD(i,d)                    \
-do {                                \
-  ieee_double_shape_type gh_u;                    \
-  gh_u.value = (d);                        \
-  (i) = gh_u.parts.msw;                        \
-} while (0)
+#define GET_HIGH_WORD(i,d)                              \
+    do {                                                \
+        ieee_double_shape_type gh_u;                    \
+        gh_u.value = (d);                               \
+        (i) = gh_u.parts.msw;                           \
+    } while (0)
 
 /* Get the less significant 32 bit int from a double.  */
 
-#define GET_LOW_WORD(i,d)                    \
-do {                                \
-  ieee_double_shape_type gl_u;                    \
-  gl_u.value = (d);                        \
-  (i) = gl_u.parts.lsw;                        \
-} while (0)
+#define GET_LOW_WORD(i,d)                               \
+    do {                                                \
+        ieee_double_shape_type gl_u;                    \
+        gl_u.value = (d);                               \
+        (i) = gl_u.parts.lsw;                           \
+    } while (0)
 
 /* Set a double from two 32 bit ints.  */
 
-#define INSERT_WORDS(d,ix0,ix1)                    \
-do {                                \
-  ieee_double_shape_type iw_u;                    \
-  iw_u.parts.msw = (ix0);                    \
-  iw_u.parts.lsw = (ix1);                    \
-  (d) = iw_u.value;                        \
-} while (0)
+#define INSERT_WORDS(d,ix0,ix1)                         \
+    do {                                                \
+        ieee_double_shape_type iw_u;                    \
+        iw_u.parts.msw = (ix0);                         \
+        iw_u.parts.lsw = (ix1);                         \
+        (d) = iw_u.value;                               \
+    } while (0)
 
 /* Set the more significant 32 bits of a double from an int.  */
 
-#define SET_HIGH_WORD(d,v)                    \
-do {                                \
-  ieee_double_shape_type sh_u;                    \
-  sh_u.value = (d);                        \
-  sh_u.parts.msw = (v);                        \
-  (d) = sh_u.value;                        \
-} while (0)
+#define SET_HIGH_WORD(d,v)                              \
+    do {                                                \
+        ieee_double_shape_type sh_u;                    \
+        sh_u.value = (d);                               \
+        sh_u.parts.msw = (v);                           \
+        (d) = sh_u.value;                               \
+    } while (0)
 
 /* Set the less significant 32 bits of a double from an int.  */
 
-#define SET_LOW_WORD(d,v)                    \
-do {                                \
-  ieee_double_shape_type sl_u;                    \
-  sl_u.value = (d);                        \
-  sl_u.parts.lsw = (v);                        \
-  (d) = sl_u.value;                        \
-} while (0)
+#define SET_LOW_WORD(d,v)                               \
+    do {                                                \
+        ieee_double_shape_type sl_u;                    \
+        sl_u.value = (d);                               \
+        sl_u.parts.lsw = (v);                           \
+        (d) = sl_u.value;                               \
+    } while (0)
 
 /* A union which permits us to convert between a float and a 32 bit
    int.  */
 
-typedef union
-{
-  float value;
-  __uint32_t word;
+typedef union {
+    float value;
+    __uint32_t word;
 } ieee_float_shape_type;
 
 /* Get a 32 bit int from a float.  */
 
-#define GET_FLOAT_WORD(i,d)                    \
-do {                                \
-  ieee_float_shape_type gf_u;                    \
-  gf_u.value = (d);                        \
-  (i) = gf_u.word;                        \
-} while (0)
+#define GET_FLOAT_WORD(i,d)                             \
+    do {                                                \
+        ieee_float_shape_type gf_u;                     \
+        gf_u.value = (d);                               \
+        (i) = gf_u.word;                                \
+    } while (0)
 
 /* Set a float from a 32 bit int.  */
 
-#define SET_FLOAT_WORD(d,i)                    \
-do {                                \
-  ieee_float_shape_type sf_u;                    \
-  sf_u.word = (i);                        \
-  (d) = sf_u.value;                        \
-} while (0)
+#define SET_FLOAT_WORD(d,i)                             \
+    do {                                                \
+        ieee_float_shape_type sf_u;                     \
+        sf_u.word = (i);                                \
+        (d) = sf_u.value;                               \
+    } while (0)
 
 /* Macros to avoid undefined behaviour that can arise if the amount
    of a shift is exactly equal to the size of the shifted operand.  */
 
-#define SAFE_LEFT_SHIFT(op,amt)                    \
-  (((amt) < 8 * sizeof(op)) ? ((op) << (amt)) : 0)
+#define SAFE_LEFT_SHIFT(op,amt)                         \
+    (((amt) < 8 * sizeof(op)) ? ((op) << (amt)) : 0)
 
-#define SAFE_RIGHT_SHIFT(op,amt)                \
-  (((amt) < 8 * sizeof(op)) ? ((op) >> (amt)) : 0)
+#define SAFE_RIGHT_SHIFT(op,amt)                        \
+    (((amt) < 8 * sizeof(op)) ? ((op) >> (amt)) : 0)
 
 #ifdef  _COMPLEX_H
 
@@ -143,22 +138,21 @@ do {                                \
  * imaginary part, of the complex number.
  */
 typedef union {
-        float complex z;
-        float parts[2];
+    float complex z;
+    float parts[2];
 } float_complex;
 
 typedef union {
-        double complex z;
-        double parts[2];
+    double complex z;
+    double parts[2];
 } double_complex;
 
 typedef union {
-        long double complex z;
-        long double parts[2];
+    long double complex z;
+    long double parts[2];
 } long_double_complex;
 
 #define REAL_PART(z)    ((z).parts[0])
 #define IMAG_PART(z)    ((z).parts[1])
 
 #endif  /* _COMPLEX_H */
-
