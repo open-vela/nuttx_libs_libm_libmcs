cfb4806d3950faf4f84388bbfefa301b1634ece0
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2021-02-05 16:12:17 +0100

    Update for new directory structure

--- lint-libm/lint.sh
+++ lint-libm/lint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-find ../libm/common -name  "*.c" | xargs grep -L "l.c" > result/files.lnt
-find ../libm/math -name  "*.c" >> result/files.lnt
+find ../libm/common -name  "*.c" | xargs grep -L "fenv.c" > result/files.lnt
+find ../libm/complexd -name  "*.c" >> result/files.lnt
+find ../libm/complexf -name  "*.c" >> result/files.lnt
+find ../libm/mathd -name  "*.c" >> result/files.lnt
+find ../libm/mathf -name  "*.c" >> result/files.lnt
 
-lint-nt.exe std.lnt result/files.lnt | tr "\\\\" "/" | tr -d '\r' | sed 's/ \[MISRA/$ \[MISRA/' | tee result/lint.txt | grep -e "--- Module"
+./lint-nt.exe std.lnt result/files.lnt | tr "\\\\" "/" | tr -d '\r' | sed 's/ \[MISRA/$ \[MISRA/' | tee result/lint.txt | grep -e "--- Module"
 cat result/lint.txt | sort | uniq | grep mandatory > result/mandatory.txt
 cat result/lint.txt | sort | uniq | grep required > result/required.txt
 cat result/lint.txt | sort | uniq | grep advisory > result/advisory.txt
