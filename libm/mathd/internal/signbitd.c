d6948f91f2147dfaebeded394bb81405bff501c0
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2021-02-24 14:23:09 +0100

    Add classification macros to individual component design

--- libm/mathd/internal/signbitd.c
+++ libm/mathd/internal/signbitd.c
@@ -1,35 +1,51 @@
 /* SPDX-License-Identifier: RedHat */
 /* Copyright (C) 2002 by  Red Hat, Incorporated. All rights reserved. */
 
-/*
-FUNCTION
-<<signbit>>---Does floating-point number have negative sign?
-
-INDEX
-    signbit
-
-SYNOPSIS
-    #include <math.h>
-    int signbit(real-floating <[x]>);
-
-DESCRIPTION
-The <<signbit>> macro determines whether the sign of its argument value is
-negative.  The macro reports the sign of all values, including infinities,
-zeros, and NaNs.  If zero is unsigned, it is treated as positive.  As shown in
-the synopsis, the argument is "real-floating," meaning that any of the real
-floating-point types (float, double, etc.) may be given to it.
-
-Note that because of the possibilities of signed 0 and NaNs, the expression
-"<[x]> < 0.0" does not give the same result as <<signbit>> in all cases.
-
-RETURNS
-The <<signbit>> macro returns a nonzero value if and only if the sign of its
-argument value is negative.
-
-PORTABILITY
-C99, POSIX.
-
-*/
+/**
+ *
+ * This macro returns a non-zero value if the sign bit of :math:`x` is set. It can be called with ``float``, ``double`` or ``long double`` input. This macro is implemented in ``math.h``.
+ *
+ * Synopsis
+ * ========
+ *
+ * .. code-block:: c
+ *
+ *     #include <math.h>
+ *     int signbit(x);
+ *
+ * Description
+ * ===========
+ *
+ * ``signbit`` returns a non-zero value if the sign bit of :math:`x` is set.
+ *
+ * Mathematical Function
+ * =====================
+ * 
+ * .. math::
+ *
+ *    signbit(x) = \left\{\begin{array}{ll} 1, & x \in \mathbb{F}^{-} \\
+ *                                          0, & otherwise \end{array}\right.
+ *
+ * Returns
+ * =======
+ *
+ * ``signbit`` returns :math:`1` if the sign bit of :math:`x` is set, otherwise :math:`0`.
+ *
+ * Exceptions
+ * ==========
+ *
+ * Does not raise exceptions.
+ *
+ * Output map
+ * ==========
+ *
+ * +---------------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+
+ * | **x**               | :math:`-NaN` | :math:`-Inf` | :math:`<0`   | :math:`-0`   | :math:`+0`   | :math:`>0`   | :math:`+Inf` | :math:`+NaN` |
+ * +=====================+==============+==============+==============+==============+==============+==============+==============+==============+
+ * | **signbit(x)**      | :math:`1`                                                 | :math:`0`                                                 |
+ * +---------------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+
+ *
+ *///
 
 #include <math.h>
 #include "../../common/tools.h"
