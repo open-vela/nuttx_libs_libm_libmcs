3d5c9f275bc98e45ca6e2d10ec3e25e3416e6666
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2021-04-09 16:59:31 +0200

    log10/f: Improve by using internal logarithm functions, in accordance with FreeBSD, commit: https://github.com/freebsd/freebsd/commit/763a6159cfdbe9ac1004795e4dc32e7718d32769 and modified by https://github.com/freebsd/freebsd/commit/2772ec758e6fb6c0bdaa3e30a268816e754de5a7

--- libm/mathf/log10f.c
+++ libm/mathf/log10f.c
@@ -4,18 +4,20 @@
 
 #include <math.h>
 #include "../common/tools.h"
+#include "internal/log1pmff.h"
 
 static const float
-two25      =  3.3554432000e+07, /* 0x4c000000 */
-ivln10     =  4.3429449201e-01, /* 0x3ede5bd9 */
-log10_2hi  =  3.0102920532e-01, /* 0x3e9a2080 */
-log10_2lo  =  7.9034151668e-07; /* 0x355427db */
+two25      =  3.3554432000e+07f, /* 0x4c000000 */
+ivln10hi   =  4.3432617188e-01f, /* 0x3ede6000 */
+ivln10lo   = -3.1689971365e-05f, /* 0xb804ead9 */
+log10_2hi  =  3.0102920532e-01f, /* 0x3e9a2080 */
+log10_2lo  =  7.9034151668e-07f; /* 0x355427db */
 
-static const float zero   =  0.0;
+static const float zero = 0.0f;
 
 float log10f(float x)
 {
-    float y, z;
+    float f, hfsq, hi, lo, r, y;
     int32_t i, k, hx;
 
     GET_FLOAT_WORD(hx, x);
@@ -40,13 +42,24 @@ float log10f(float x)
         GET_FLOAT_WORD(hx, x);
     }
 
+    if (hx == 0x3f800000) { /* log(1) = +0 */
+        return zero;
+    }
+
     k += (hx >> 23) - 127;
-    i  = ((uint32_t)k & 0x80000000) >> 31;
-    hx = (hx & 0x007fffff) | ((0x7f - i) << 23);
-    y  = (float)(k + i);
-    SET_FLOAT_WORD(x, hx);
-    z  = y * log10_2lo + ivln10 * logf(x);
-    return  z + y * log10_2hi;
+    hx &= 0x007fffff;
+    i = (hx + (0x4afb0d)) & 0x800000;
+    SET_FLOAT_WORD(x, hx | (i ^ 0x3f800000)); /* normalize x or x/2 */
+    k += (i >> 23);
+    y = (float)k;
+    f = x - 1.0f;
+    hfsq = 0.5f * f * f;
+    r = __log1pmff(f);
+    hi = f - hfsq;
+    GET_FLOAT_WORD(hx, hi);
+    SET_FLOAT_WORD(hi, hx & 0xfffff000);
+    lo = (f - hi) - hfsq + r;
+    return y * log10_2lo + (lo + hi) * ivln10lo + lo * ivln10hi + hi * ivln10hi + y * log10_2hi;
 }
 
 #ifdef __LIBMCS_DOUBLE_IS_32BITS
