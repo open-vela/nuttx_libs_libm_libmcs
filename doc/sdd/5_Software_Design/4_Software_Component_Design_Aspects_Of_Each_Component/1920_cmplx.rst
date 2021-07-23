5287b993f1ba8e24943c312a3311780f2bdcd94f
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2021-07-23 17:09:12 +0200

    Docu: Add complex manipulation functions to individual component design

---
+++ doc/sdd/5_Software_Design/4_Software_Component_Design_Aspects_Of_Each_Component/1920_cmplx.rst
@@ -0,0 +1,19 @@
+CMPLX
+~~~~~~~~~
+
+.. c:autodoc:: ../libm/common/cmplx.c
+
+Mathematical Approach
+^^^^^^^^^^^^^^^^^^^^^
+
+Here there be dragons. (TODO)
+
+Requirements
+^^^^^^^^^^^^
+
+* REQ-ML-7900
+
+References
+^^^^^^^^^^
+
+Nearly all complex procedures use ``CMPLX``.
--- libm/complexd/creald.c
+++ libm/complexd/creald.c
@@ -1,44 +1,38 @@
 /* SPDX-License-Identifier: PublicDomain */
 /* Written by Matthias Drochner <drochner@NetBSD.org>. */
 
-/*
-FUNCTION
-        <<creal>>, <<crealf>>, <<creall>>---real part
-
-INDEX
-        creal
-INDEX
-        crealf
-INDEX
-        creall
-
-SYNOPSIS
-       #include <complex.h>
-       double creal(double complex <[z]>);
-       float crealf(float complex <[z]>);
-       double long creall(long double complex <[z]>);
-
-
-DESCRIPTION
-        These functions compute the real part of <[z]>.
-
-        <<crealf>> is identical to <<creal>>, except that it performs
-        its calculations on <<float complex>>.
-
-        <<creall>> is identical to <<creal>>, except that it performs
-        its calculations on <<long double complex>>.
-
-RETURNS
-        The creal* functions return the real part value.
-
-PORTABILITY
-        <<creal>>, <<crealf>> and <<creall>> are ISO C99
-
-QUICKREF
-        <<creal>>, <<crealf>> and <<creall>> are ISO C99
-
-*/
-
+/**
+ *
+ * This family of functions returns the real part of :math:`z` as a real.
+ *
+ * Synopsis
+ * ========
+ *
+ * .. code-block:: c
+ *
+ *     #include <complex.h>
+ *     float crealf(float complex z);
+ *     double creal(double complex z);
+ *     long double creall(long double complex z);
+ *
+ * Description
+ * ===========
+ *
+ * ``creal`` computes the real part of :math:`z` as a real.
+ *
+ * Mathematical Function
+ * =====================
+ * 
+ * .. math::
+ *
+ *    creal(z) = \Re(z)
+ *
+ * Returns
+ * =======
+ *
+ * ``creal`` returns the real part of :math:`z` as a real.
+ * 
+ *///
 
 #include <complex.h>
 #include "../common/tools.h"
