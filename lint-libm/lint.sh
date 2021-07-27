42596fba32936b9ce4091cdfc9054067d133925e
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2021-07-27 15:55:08 +0200

    lint: Make lint.sh configurable

--- lint-libm/lint.sh
+++ lint-libm/lint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Point to your pc-lint executeable
+LINT=./lint-nt.exe
+
 find ../libm/common -name  "*.c" | xargs grep -L "fenv.c" > result/files.lnt
 find ../libm/complexd -name  "*.c" >> result/files.lnt
 find ../libm/complexf -name  "*.c" >> result/files.lnt
 find ../libm/mathd -name  "*.c" >> result/files.lnt
 find ../libm/mathf -name  "*.c" >> result/files.lnt
 
-./lint-nt.exe std.lnt result/files.lnt | tr "\\\\" "/" | tr -d '\r' | sed 's/ \[MISRA/$ \[MISRA/' | tee result/lint.txt | grep -e "--- Module"
+${LINT} std.lnt result/files.lnt | tr "\\\\" "/" | tr -d '\r' | sed 's/ \[MISRA/$ \[MISRA/' | tee result/lint.txt | grep -e "--- Module"
 cat result/lint.txt | sort | uniq | grep mandatory > result/mandatory.txt
 cat result/lint.txt | sort | uniq | grep required > result/required.txt
 cat result/lint.txt | sort | uniq | grep advisory > result/advisory.txt
