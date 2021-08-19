cd755afb2cffb8674528c0791174f15b7593e929
Fabian Schriever <fabian.schriever@gtd-gmbh.de>
2021-08-19 12:33:21 +0200

    Add inline issignaling functions

--- libm/mathf/fminf.c
+++ libm/mathf/fminf.c
@@ -2,21 +2,34 @@
 /* Copyright (C) 2002 by  Red Hat, Incorporated. All rights reserved. */
 
 #include <math.h>
+#include "../common/tools.h"
 
 float fminf(float x, float y)
 {
-#ifdef __LIBMCS_FPU_DAZ
-    x *= __volatile_onef;
-    y *= __volatile_onef;
-#endif /* defined(__LIBMCS_FPU_DAZ) */
-
     if (isnan(x)) {
+        if (__issignalingf(x) != 0 || __issignalingf(y) != 0) {
+            return x * y;
+        }
+#ifdef __LIBMCS_FPU_DAZ
+        y *= __volatile_onef;
+#endif /* defined(__LIBMCS_FPU_DAZ) */
         return y;
     }
 
     if (isnan(y)) {
+        if (__issignalingf(x) != 0 || __issignalingf(y) != 0) {
+            return x * y;
+        }
+#ifdef __LIBMCS_FPU_DAZ
+        x *= __volatile_onef;
+#endif /* defined(__LIBMCS_FPU_DAZ) */
         return x;
     }
+    
+#ifdef __LIBMCS_FPU_DAZ
+    x *= __volatile_onef;
+    y *= __volatile_onef;
+#endif /* defined(__LIBMCS_FPU_DAZ) */
 
     return x < y ? x : y;
 }
