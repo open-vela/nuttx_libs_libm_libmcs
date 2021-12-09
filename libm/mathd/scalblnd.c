f386e3f49b0ff4e134034398c33ff95c834e0b01
openvela-robot <openvela-robot@xiaomi.com>
2021-12-09 11:34:17 +0800

    Merge branch '63-changes-due-to-isvv-from-08-2021' into 'development'
    Resolve "Changes due to ISVV from 08.2021"
    
    See merge request e1356-mathematical-library-qualification/qlibmcs!73

--- libm/mathd/scalblnd.c
+++ libm/mathd/scalblnd.c
@@ -99,14 +99,10 @@ double scalbln(double x, long int n)
         return x + x;               /* NaN or Inf */
     }
 
-    if (n > 50000) {
-        return __raise_overflow(x);     /*overflow*/
-    }
-
     k = k + n;
 
-    if (k >  0x7fe) {
-        return __raise_overflow(x);     /*overflow*/
+    if (n > 50000 || k >  0x7fe) {
+        return __raise_overflow(x);    /*overflow*/
     }
 
     if (n < -50000) {
--- libm/mathd/scalbnd.c
+++ libm/mathd/scalbnd.c
@@ -105,10 +105,6 @@ double scalbn(double x, int n)
         return x + x;             /* NaN or Inf */
     }
 
-    if (n > 50000) {
-        return __raise_overflow(x);         /*overflow */
-    }
-
     k = k + n;
 
     if (k >  0x7fe) {
@@ -121,7 +117,11 @@ double scalbn(double x, int n)
     }
 
     if (k <= -54) {
-        return __raise_underflow(x);        /*underflow*/
+        if (n > 50000) {          /* in case integer overflow in n+k */
+            return __raise_overflow(x);     /*overflow */
+        } else {
+            return __raise_underflow(x);    /*underflow*/
+        }
     }
 
     k += 54;                      /* subnormal result */
