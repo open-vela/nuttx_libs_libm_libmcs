8e96d4597524b1a001930481a75026b598947446
Andoni Arregi <andoni.arregui@gtd-gmbh.de>
2022-06-09 09:54:31 +0200

    Bound the recompute loop to max precision of ipio2

--- libm/mathf/internal/trigf.c
+++ libm/mathf/internal/trigf.c
@@ -109,8 +109,6 @@ static inline int __rem_pio2f_internal(float *x, float *y, int e0, int nx)
     bool recompute;   /* variable used to signalize that a recomputation is needed as the current selection of ipio2[] terms has led to loss of significance.
                          The recomputation will take more terms of ipio2[]. */
 
-    /* initialize jk*/
-    jk = 7;
     jp = jk;
 
     /* determine jx,jv,q0, note that 3>q0 */
@@ -139,6 +137,7 @@ static inline int __rem_pio2f_internal(float *x, float *y, int e0, int nx)
     jz = jk;
     do {
         recompute = false;
+        exhausted = false;
 
         /* distill the lower part of q[] into iq[] reversingly and leave the higher part in z */
         for (i = 0, j = jz, z = q[jz]; j > 0; i++, j--) {
@@ -213,7 +212,7 @@ static inline int __rem_pio2f_internal(float *x, float *y, int e0, int nx)
         }
 
         /* check if recomputation is needed in case of loss of significance in z and iq[] */
-        if (z == zero) {
+        if (z == zero && !exhausted) {
             j = 0;
 
             for (i = jz - 1; i >= jk; i--) {
@@ -221,11 +220,15 @@ static inline int __rem_pio2f_internal(float *x, float *y, int e0, int nx)
             }
 
             if (j == 0) { /* need recomputation */
-                for (k = 1; iq[jk - k] == 0; k++) { /* k = no. of terms needed */
+                for (k = 1; (jk - k >= 0) && (iq[jk - k] == 0); k++) { /* k = no. of terms needed */
                 }
 
                 for (i = jz + 1; i <= jz + k; i++) { /* add q[jz+1] to q[jz+k] */
-                    f[jx + i] = (float) ipio2[jv + i];
+                    if (jv + i < 198) { /* don't pull more terms of ipio2[] than available */
+                        f[jx + i] = (float) ipio2[jv + i];
+                    } else{
+                        exhausted = true;
+                    }
 
                     for (j = 0, fw = 0.0f; j <= jx; j++) {
                         fw += x[j] * f[jx + i - j];
@@ -238,7 +241,9 @@ static inline int __rem_pio2f_internal(float *x, float *y, int e0, int nx)
                 recompute = true;
             }
         }
-    } while (recompute);
+    } while (recompute); /* The original authors of the algorithm Payne and Hanek estimate the
+                            amount of needed recomputing to be low. Currently only 2 recomputes
+                            are observed at most */
 
     /* chop off zero terms */
     if (z == 0.0f) {
