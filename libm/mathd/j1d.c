dbe76d4bcfaf1a86b102df37f8b9cae7a04efeec
liujinye <liujinye@xiaomi.com>
2022-07-29 13:23:13 +0800

    Merge branch 'ISVV-12-substitute-architecture-figure' into 'development'
    ISVV 12 substitute architecture figure
    
    See merge request gtd-gmbh/libmcs!102

--- libm/mathd/j1d.c
+++ libm/mathd/j1d.c
@@ -41,7 +41,7 @@
  * Exceptions
  * ==========
  *
- * Does not raise overflow, division by zero, and invalid exceptions.
+ * Does not raise useful exceptions.
  *
  * .. May raise ``underflow`` exception.
  *
