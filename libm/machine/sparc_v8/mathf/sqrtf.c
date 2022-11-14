7bf4221cfbff4399184f6ced8871603a0e551c9c
Thomas Wucher <thomas.wucher@gtd-gmbh.de>
2022-11-14 14:17:18 +0100

    Do not use asm GNU extension

--- libm/machine/sparc_v8/mathf/sqrtf.c
+++ libm/machine/sparc_v8/mathf/sqrtf.c
@@ -7,9 +7,9 @@ float sqrtf(float x)
 {
     float root = 0.0f;
 
-    asm volatile ("fsqrts %[x], %[root]"
-                  : [root] "=f" (root)
-                  : [x] "f" (x));
+    __asm__ volatile ("fsqrts %[x], %[root]"
+                      : [root] "=f" (root)
+                      : [x] "f" (x));
 
     return root;
 }
